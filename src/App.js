53a65cd0ccd2eea96d442d94e98199da3a551f52
lexsander ikhaylov <mihaleksval@gmail.com>
2021-07-07 15:19:54 +0500

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class MarkDown extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      edit: [],
+      preview: ""
+    };
+  }
+  render() {
+    return (
+      <body className="body">
+        <div
+          className='container-fluid d-flex align-items-center justify-content-center'
+          id='editor'
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <div className='d-flex card' id=''>
+            <div className='card-body' id=''>
+              <div className='card-header card-edit'>
+                <h4>Editor</h4>
+              </div>
+              <textarea 
+              value={this.state.preview}
+              >
+                <p>Hello there!!!</p>
+              </textarea>
+            </div>
+          </div>
+        </div>
+        <div
+          className='container-fluid d-flex align-items-center justify-content-center'
+          id='preview'
+        >
+          <div className='d-flex card card-preview' id=''>
+            <div className='card-body' id=''>
+              <div className='card-header card-preview'>
+                <h4>Preview</h4>
+              </div>
+              <textarea className="preview-textarea">
+                <p>Hello there!!!</p>
+              </textarea>
+            </div>
+          </div>
+        </div>
+      </body>
+    );
+  }
 }
 
-export default App;
+export default MarkDown;
